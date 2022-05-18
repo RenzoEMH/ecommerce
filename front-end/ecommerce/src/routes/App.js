f85855701990bf6bcf9ab4afeb358eb078254d05
Renzo <renzmanri97@gmail.com>
2022-05-17 19:05:59 -0500

    Update routes and add new dependencies

--- front-end/ecommerce/src/routes/App.js
+++ front-end/ecommerce/src/routes/App.js
@@ -1,5 +1,7 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import parseJwt from "../utils/ParseJwt";
 import Home from "../pages/general/Home";
 import Login from "../pages/general/Login";
 import Register from "../pages/general/Register";
@@ -8,13 +10,21 @@ import ShopCart from "../pages/user/Shopcart";
 import SearchProduct from "../pages/user/SearchProduct";
 import NavbarMain from "../components/navbar/Navbar";
 import Store from "../pages/user/Store";
+import EditProfile from "../pages/user/EditProfile";
+import DetailProduct from "../pages/user/DetailProducts";
 
 function App() {
+  const token = useSelector((state) => state.usuarios.token);
+  const sesion = parseJwt(token);
   return (
     <div className="App">
       <NavbarMain />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route
+          exact
+          path="/"
+          element={sesion?.type === "admin" ? <Profile /> : <Home />}
+        />
       </Routes>
       <Routes>
         <Route exact path="/register" element={<Register />} />
@@ -24,6 +34,12 @@ function App() {
       </Routes>
       <Routes>
         <Route exact path="/profile" element={<Profile />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/profile_edit" element={<EditProfile />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/details/:productId" element={<DetailProduct />} />
       </Routes>
       <Routes>
         <Route exact path="/shopcart" element={<ShopCart />} />
