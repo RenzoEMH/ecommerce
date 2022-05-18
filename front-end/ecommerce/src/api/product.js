f594e4ffb9b066d16e86a12c09e16a13a202bc54
Renzo <renzmanri97@gmail.com>
2022-05-17 19:04:23 -0500

    Integration with back-end

---
+++ front-end/ecommerce/src/api/product.js
@@ -0,0 +1,34 @@
+const API_SERVER = "http://localhost:5000";
+
+const ENDPOINTS = {
+  GET_ALL: "/api/product",
+  GET_ONE: "/api/product",
+};
+
+export const getAllProducts = () => {
+  const path = `${API_SERVER}${ENDPOINTS.GET_ALL}`;
+  return new Promise((resolve, reject) => {
+    fetch(path)
+      .then((response) => response.json())
+      .then((data) => {
+        resolve({ data });
+      })
+      .catch((err) => {
+        reject({ error: err });
+      });
+  });
+};
+
+export const getOneProduct = (id) => {
+  const path = `${API_SERVER}${ENDPOINTS.GET_ONE}/${id}`;
+  return new Promise((resolve, reject) => {
+    fetch(path)
+      .then((response) => response.json())
+      .then((data) => {
+        resolve({ data });
+      })
+      .catch((err) => {
+        reject({ error: err });
+      });
+  });
+};
---
+++ front-end/ecommerce/src/api/users.js
@@ -0,0 +1,99 @@
+const API_SERVER = "http://localhost:5000";
+
+const ENDPOINTS = {
+  GET_ALL: "/api/user",
+  GET_ONE: "/api/user",
+  CREATE: "/api/user/register",
+  LOGIN: "/api/auth/local/login",
+  GET_ONE: "/api/user",
+  UPDATE: "/api/user",
+};
+
+export const getAllUsers = () => {
+  const path = `${API_SERVER}${ENDPOINTS.GET_ALL}`;
+  return new Promise((resolve, reject) => {
+    fetch(path)
+      .then((response) => response.json())
+      .then((data) => {
+        resolve({ data });
+      })
+      .catch((err) => {
+        reject({ error: err });
+      });
+  });
+};
+
+export const getOneUser = (id) => {
+  const path = `${API_SERVER}${ENDPOINTS.GET_ONE}/${id}`;
+  return new Promise((resolve, reject) => {
+    fetch(path)
+      .then((response) => response.json())
+      .then((data) => {
+        resolve({ data });
+      })
+      .catch((err) => {
+        reject({ error: err });
+      });
+  });
+};
+
+export const createUser = (user) => {
+  const path = `${API_SERVER}${ENDPOINTS.CREATE}`;
+  return new Promise((resolve, reject) => {
+    fetch(path, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        resolve({ data });
+      })
+      .catch((err) => {
+        reject({ error: err });
+      });
+  });
+};
+
+export const updateUser = ({ id, ...user }) => {
+  const path = `${API_SERVER}${ENDPOINTS.UPDATE}/${id}`;
+  return new Promise((resolve, reject) => {
+    fetch(path, {
+      method: "PUT",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(true);
+      })
+      .catch((err) => {
+        reject({ error: err });
+      });
+  });
+};
+
+export const login = (user) => {
+  const path = `${API_SERVER}${ENDPOINTS.LOGIN}`;
+  return new Promise((resolve, reject) => {
+    fetch(path, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // resolve({ token: data.token });
+        resolve({ data });
+      })
+      .catch((err) => {
+        reject({ error: err });
+      });
+  });
+};
